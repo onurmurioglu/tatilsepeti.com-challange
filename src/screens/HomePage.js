ee751888a3130edf0d1415af9093334a8d941506
onurmurioglu <107471485+onurmurioglu@users.noreply.github.com>
2024-01-10 11:05:14 +0300

    Update HomePage.js

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -6,234 +7,256 @@ import {
   ImageBackground,
   TouchableOpacity,
   ActivityIndicator,
-  Touchable,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import axios from "axios";
 import { FlashList } from "@shopify/flash-list";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-const HotelCard = ({ hotel }) => {
-  const [isFavourite, setIsFavourite] = useState(false);
+class HotelCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFavourite: false,
+    };
+  }
 
-  return (
-    <View style={styles.cardContainer}>
-      <ImageBackground
-        source={{ uri: hotel.photoPath }}
-        style={styles.cardImage}
-      >
-        {isFavourite ? (
-          <TouchableOpacity
-            style={styles.favouriteIcon}
-            onPress={() => {
-              setIsFavourite(false);
+  render() {
+    const { hotel } = this.props;
+    const { isFavourite } = this.state;
+
+    return (
+      <View style={styles.cardContainer}>
+        <ImageBackground
+          source={{ uri: hotel.photoPath }}
+          style={styles.cardImage}
+        >
+          {isFavourite ? (
+            <TouchableOpacity
+              style={styles.favouriteIcon}
+              onPress={() => {
+                this.setState({ isFavourite: false });
+              }}
+            >
+              <Icon name="heart" size={30} color="red" />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.setState({ isFavourite: true })}
+            >
+              <Icon
+                name="heart-outline"
+                size={30}
+                color="white"
+                style={{ top: 10, left: 7 }}
+              />
+            </TouchableOpacity>
+          )}
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              fontSize: 18,
+              alignSelf: "flex-end",
+              padding: 10,
+              right: 8,
+              backgroundColor: hotel.customerScore < 9.0 ? "orange" : "#738435",
+              borderRadius: 8,
+              height: "20%",
+              bottom: 20,
             }}
           >
-            <Icon name="heart" size={30} color="red" />
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity onPress={() => setIsFavourite(true)}>
-            <Icon
-              name="heart-outline"
-              size={30}
-              color="white"
-              style={{ top: 10, left: 7 }}
-            />
-          </TouchableOpacity>
-        )}
-        <Text
-          style={{
-            color: "white",
-            fontWeight: "bold",
-            fontSize: 18,
-            alignSelf: "flex-end",
-            padding: 10,
-            right: 8,
-            backgroundColor: hotel.customerScore < 9.0 ? "orange" : "#738435",
-            borderRadius: 8,
-            height: "20%",
-            bottom: 20,
-          }}
-        >
-          {hotel.customerScore.toFixed(1)}
-        </Text>
+            {hotel.customerScore.toFixed(1)}
+          </Text>
 
-        <Text style={styles.certificateText}>
-          {hotel.hasMinistryOfHealthCertificate}
-        </Text>
-      </ImageBackground>
+          <Text style={styles.certificateText}>
+            {hotel.hasMinistryOfHealthCertificate}
+          </Text>
+        </ImageBackground>
 
-      <Text style={styles.cardText}>{hotel.hotelName}</Text>
+        <Text style={styles.cardText}>{hotel.hotelName}</Text>
 
-      <View style={styles.infoContainer}>
-        <Icon
-          name="pin"
-          size={20}
-          color="#53A8DB"
-          style={styles.locationIcon}
-        />
-        <Text style={styles.locationText}>
-          {hotel.areaName} - {hotel.subAreaName} - {hotel.subAreaDetailName}
-        </Text>
+        <View style={styles.infoContainer}>
+          <Icon
+            name="pin"
+            size={20}
+            color="#53A8DB"
+            style={styles.locationIcon}
+          />
+          <Text style={styles.locationText}>
+            {hotel.areaName} - {hotel.subAreaName} - {hotel.subAreaDetailName}
+          </Text>
+
+          <View
+            style={{
+              right: 30,
+              alignItems: "center",
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              {hotel.price.toFixed(2) !== hotel.discountPrice.toFixed(2) && (
+                <>
+                  <Text style={styles.currencyText}>₺ </Text>
+                  <Text style={styles.priceText}>
+                    {hotel.price.toFixed(2).replace(".", ",")}
+                  </Text>
+                </>
+              )}
+            </View>
+
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.discountPriceCurrency}>₺ </Text>
+
+              <Text style={styles.discountPriceText}>
+                <Text style={{ fontWeight: "600" }}>
+                  {hotel.discountPrice.toFixed(2).split(".")[0]}
+                </Text>
+                <Text style={{ fontSize: 16 }}>
+                  {"," + hotel.discountPrice.toFixed(2).split(".")[1]}
+                </Text>
+              </Text>
+            </View>
+
+            <Text style={{ color: "#B3B3BD" }}>gecelik kişi başı</Text>
+          </View>
+        </View>
 
         <View
           style={{
-            right: 30,
+            borderWidth: 1,
+            borderRadius: 5,
+            width: Math.min(hotel.accommodation.length * 8, 300),
             alignItems: "center",
+            justifyContent: "center",
+            height: 30,
+            padding: 2,
+            left: 15,
+            bottom: 37,
+            marginTop: "2%",
           }}
         >
-          <View style={{ flexDirection: "row" }}>
-            {hotel.price.toFixed(2) !== hotel.discountPrice.toFixed(2) && (
-              <>
-                <Text style={styles.currencyText}>₺ </Text>
-                <Text style={styles.priceText}>
-                  {hotel.price.toFixed(2).replace(".", ",")}
-                </Text>
-              </>
-            )}
-          </View>
+          <Text style={styles.accommodation} numberOfLines={1}>
+            {hotel.accommodation}
+          </Text>
+        </View>
 
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.discountPriceCurrency}>₺ </Text>
-
-            <Text style={styles.discountPriceText}>
-              <Text style={{ fontWeight: "600" }}>
-                {hotel.discountPrice.toFixed(2).split(".")[0]}
-              </Text>
-              <Text style={{ fontSize: 16 }}>
-                {"," + hotel.discountPrice.toFixed(2).split(".")[1]}
-              </Text>
-            </Text>
-          </View>
-
-          <Text style={{ color: "#B3B3BD" }}>gecelik kişi başı</Text>
+        <View
+          style={{ flexDirection: "row", bottom: 30, left: 8, marginTop: "1%" }}
+        >
+          <Icon
+            name="brightness-percent"
+            size={20}
+            color="#6BC079"
+            style={{ left: 8, top: 2 }}
+          />
+          <Text style={styles.campaignName}>{hotel.campaignName}</Text>
         </View>
       </View>
+    );
+  }
+}
 
-      <View
-        style={{
-          borderWidth: 1,
-          borderRadius: 5,
-          // width: "35%",
-          width: Math.min(hotel.accommodation.length * 8, 300),
-          alignItems: "center",
-          justifyContent: "center",
-          height: 30,
-          padding: 2,
-          left: 15,
-          bottom: 37,
-          marginTop: "2%",
-        }}
-      >
-        <Text style={styles.accommodation} numberOfLines={1}>
-          {hotel.accommodation}
-        </Text>
-      </View>
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotelData: [],
+      loading: false,
+      page: 1,
+      pageSize: 5,
+      totalPages: null,
+    };
+  }
 
-      <View
-        style={{ flexDirection: "row", bottom: 30, left: 8, marginTop: "1%" }}
-      >
-        <Icon
-          name="brightness-percent"
-          size={20}
-          color="#6BC079"
-          style={{ left: 8, top: 2 }}
-        />
-        <Text style={styles.campaignName}>{hotel.campaignName}</Text>
-      </View>
-    </View>
-  );
-};
+  componentDidMount() {
+    this.fetchData();
+  }
 
-const HomePage = () => {
-  const [hotelData, setHotelData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(5);
-  const [totalPages, setTotalPages] = useState(null);
+  fetchData = async () => {
+    const { loading, page, pageSize } = this.state;
+    if (loading) return;
 
-  useEffect(() => {
-    fetchData();
-  }, [page]);
+    this.setState({ loading: true });
 
-  const fetchData = async () => {
-    setLoading(true);
     try {
       const response = await axios.get(
         `https://gist.githubusercontent.com/yasaricli/de2282f01c739a5c8fcbffbb9116e277/raw/949b2393642747d2f54edf0ce659f27a69c87690/hotels.json?page=${page}&pageSize=${pageSize}`
       );
 
-      //setTotalPages(response.data.resultObject.totalPages);
-
-      setTotalPages(response.data.resultObject);
-      console.log("Result: ", response.data.resultObject);
-
-      if (page === 1) {
-        setHotelData(response.data.resultObject.hotelList);
-      } else {
-        setHotelData((prevData) => [
-          ...prevData,
-          ...response.data.resultObject.hotelList,
-        ]);
-      }
-
-      setLoading(false);
+      this.setState((prevState) => ({
+        totalPages: response.data.resultObject.totalPages,
+        hotelData:
+          page === 1
+            ? response.data.resultObject.hotelList
+            : [...prevState.hotelData, ...response.data.resultObject.hotelList],
+        loading: false,
+      }));
     } catch (error) {
       console.error("Error fetching data: ", error);
-      setLoading(false);
+      this.setState({ loading: false });
     }
   };
 
-  const handleLoadMore = () => {
+  handleLoadMore = () => {
+    const { page, totalPages } = this.state;
     if (page < totalPages) {
-      setPage(page + 1);
+      this.setState(
+        (prevState) => ({ page: prevState.page + 1 }),
+        this.fetchData
+      );
     }
   };
 
-  const Seperator = () => (
+  Seperator = () => (
     <View style={styles.seperatorContainer}>
       <View style={styles.seperator} />
     </View>
   );
 
-  return (
-    <SafeAreaProvider>
-      <View style={styles.seperatorView}>
-        <TouchableOpacity style={styles.sortBtn}>
-          <Icon
-            name="sort"
-            size={20}
-            color="#52A8DB"
-            style={{ marginRight: 8 }}
-          />
-          <Text style={styles.sortFilterBtnText}>Sırala</Text>
-        </TouchableOpacity>
+  render() {
+    const { hotelData, loading } = this.state;
 
-        <Seperator />
+    return (
+      <SafeAreaProvider>
+        <View style={styles.seperatorView}>
+          <TouchableOpacity style={styles.sortBtn}>
+            <Icon
+              name="sort"
+              size={20}
+              color="#52A8DB"
+              style={{ marginRight: 8 }}
+            />
+            <Text style={styles.sortFilterBtnText}>Sırala</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.filterBtn}>
-          <Icon
-            name="filter-variant"
-            size={20}
-            color="#52A8DB"
-            style={{ marginRight: 8 }}
-          />
-          <Text style={styles.sortFilterBtnText}>Filtrele</Text>
-        </TouchableOpacity>
-      </View>
+          <this.Seperator />
 
-      <FlashList
-        data={hotelData}
-        keyExtractor={(item) => item.hotelId.toString()}
-        renderItem={({ item }) => <HotelCard hotel={item} />}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0.1}
-        ListFooterComponent={loading && <ActivityIndicator size="large" />}
-        estimatedItemSize={380}
-      />
-    </SafeAreaProvider>
-  );
-};
+          <TouchableOpacity style={styles.filterBtn}>
+            <Icon
+              name="filter-variant"
+              size={20}
+              color="#52A8DB"
+              style={{ marginRight: 8 }}
+            />
+            <Text style={styles.sortFilterBtnText}>Filtrele</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlashList
+          data={hotelData}
+          keyExtractor={(item) => item.hotelId.toString()}
+          renderItem={({ item }) => <HotelCard hotel={item} />}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={0.1}
+          ListFooterComponent={loading && <ActivityIndicator size="large" />}
+          estimatedItemSize={380}
+        />
+      </SafeAreaProvider>
+    );
+  }
+}
+
+export default HomePage;
 
 const styles = StyleSheet.create({
   cardContainer: {
@@ -284,19 +307,6 @@ const styles = StyleSheet.create({
   accommodation: {
     color: "#204B65",
   },
-
-  // customerScore: {
-  //   color: "white",
-  //   fontWeight: "bold",
-  //   fontSize: 18,
-  //   alignSelf: "flex-end",
-  //   padding: 10,
-  //   right: 8,
-  //   backgroundColor: hotel.customerScore < 9.0 ? "orange" : "#738435",
-  //   borderRadius: 8,
-  //   height: "20%",
-  //   bottom: 20,
-  // },
 
   infoContainer: {
     width: "90%",
@@ -382,9 +392,4 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     right: 18,
   },
-  // flatListContainer: {
-  //   flexGrow: 1,
-  // },
 });
-
-export default HomePage;
