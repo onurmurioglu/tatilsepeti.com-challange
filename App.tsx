9ab79c3ea2a1bc8dc12635a78ae9948b411e8971
onurmurioglu <107471485+onurmurioglu@users.noreply.github.com>
2024-01-10 10:59:35 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+
+import React, { Component } from "react";
 import { SafeAreaView, View, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,28 +8,30 @@ import OnBoard from "./src/screens/OnBoard";
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="OnBoard"
-          component={OnBoard}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="HomePage"
-          component={HomePage}
-          options={{
-            //headerBackVisible: false,
-            headerTitle: "Antalya",
-            headerTitleAlign: "center",
-            headerTintColor: "#1C4862",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="OnBoard"
+            component={OnBoard}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="HomePage"
+            component={HomePage}
+            options={{
+              //headerBackVisible: false,
+              headerTitle: "Antalya",
+              headerTitleAlign: "center",
+              headerTintColor: "#1C4862",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
 export default App;
