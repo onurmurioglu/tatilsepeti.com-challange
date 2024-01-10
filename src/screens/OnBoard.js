d13c541edb5a5261af84d46eca83654b35111b05
onurmurioglu <107471485+onurmurioglu@users.noreply.github.com>
2024-01-10 11:14:26 +0300

    Update OnBoard.js

--- src/screens/OnBoard.js
+++ src/screens/OnBoard.js
@@ -1,48 +1,15 @@
-/*
-import { SafeAreaView, StyleSheet, Image } from "react-native";
-import React from "react";
-
-const OnBoard = ({ navigation }) => {
-  const timer = setTimeout(() => {
-    navigation.navigate("HomePage");
-    clearTimeout(timer);
-  }, 2000);
-
-  return (
-    <SafeAreaView style={styles.mainView}>
-      <Image
-        style={styles.backgroundImage}
-        source={require("../assets/ttspeti.png")}
-        resizeMode="contain"
-      />
-    </SafeAreaView>
-  );
-};
-
-export default OnBoard;
-
-const styles = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
-  },
-  backgroundImage: {
-    width: "80%",
-    height: "50%",
-  },
-});
-*/
-
 import React, { Component } from "react";
 import { SafeAreaView, StyleSheet, Image } from "react-native";
 
 class OnBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.timer = null;
+  }
+
   componentDidMount() {
     this.timer = setTimeout(() => {
       this.props.navigation.navigate("HomePage");
-      clearTimeout(this.timer);
     }, 2000);
   }
 
